ae73e24b8884c9ce595f0e4082e3e52a9b20945e
gustavodias <gustavoemersonfd@gmail.com>
2021-12-30 14:00:34 -0300

    Criando uma feature para o leilao e implementando o LeilaoSteps.

---
+++ src/test/java/br/com/alura/leilao/acceptance/steps/LeilaoSteps.java
@@ -0,0 +1,44 @@
+package br.com.alura.leilao.acceptance.steps;
+
+import br.com.alura.leilao.e2e.pages.Browser;
+import br.com.alura.leilao.e2e.pages.LeiloesPage;
+import br.com.alura.leilao.e2e.pages.LoginPage;
+import br.com.alura.leilao.e2e.pages.NovoLeilaoPage;
+import io.cucumber.java.pt.Dado;
+import io.cucumber.java.pt.Entao;
+import io.cucumber.java.pt.Quando;
+import org.junit.Assert;
+
+public class LeilaoSteps {
+
+    private Browser browser;
+    private LoginPage loginPage;
+    private LeiloesPage leiloesPage;
+    private NovoLeilaoPage novaLeilaoPage;
+
+    @Dado("um usuario logado")
+    public void um_usuario_logado() {
+        this.browser = new Browser();
+        browser.seed();
+        loginPage = browser.getLoginPage();
+        leiloesPage = loginPage.realizaLoginComoFulano();
+    }
+
+    @Quando("acessa a pagina de novo leilao")
+    public void acessa_a_pagina_de_novo_leilao() {
+        novaLeilaoPage = this.leiloesPage.visitaPaginaParaCriarUmNovoLeilao();
+    }
+    @Quando("prenche o formulario com dados validos")
+    public void prenche_o_formulario_com_dados_validos() {
+        this.leiloesPage = this.novaLeilaoPage.preencheForm("PC Gamer", "1500", "01/11/2021");
+    }
+    @Entao("volta para a pagina de leiloes")
+    public void volta_para_a_pagina_de_leiloes() {
+        Assert.assertTrue(this.leiloesPage.estaNaPaginaDeLeiloes());
+    }
+    @Entao("o novo leilao aparece na tabela")
+    public void o_novo_leilao_aparece_na_tabela() {
+        Assert.assertTrue(this.leiloesPage.existe("PC Gamer", "1500", "01/11/2021", "fulano"));
+        this.browser.clean();
+    }
+}
